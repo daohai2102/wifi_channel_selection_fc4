d79a2c4d4b503f7730a309a7b5fde888163d2826
Dao Duy Hai <duyhai.onlysea@gmail.com>
2019-03-14 10:21:56 +0700

    complete

--- cs.cc
+++ cs.cc
@@ -14,15 +14,15 @@ using namespace std;
 
 #define MAX 100
 #define NOMARK		-1
-#define NOEXIST		-2
+#define REMOVED		-2
 
 #define MAX_UTIL 0.95
 #define LIMIT_UTIL 0.9			//use to make decision
 #define MIN_ENV_UTIL 0.1
 #define MIN_TOTAL_UTIL 0.3
 
-const string topoFile = "topo.txt";
-const string currentChannelFile = "channels.txt";
+const string topoFile = "topo1.txt";
+const string currentChannelFile = "channels1.txt";
 
 struct VarValuePair{
 	int varIndex;							//variable index
@@ -56,19 +56,31 @@ public:
 	void readTopoFromFile(const string filename);
 	void readCurrentChannelFromFile(const string filename);
 	void prepareData();
+	void nodeConsistency();
+	void arcConsistency();
 	bool dwo(int varIndex);									//domain wipe out
 	bool searchFC4(list<int> vars, int level);
 	bool checkForward4(list<int> vars, int level, int varIndex, int value);
 	bool fc4();
 	void restore(list<int> vars, int level);
 	void printTopo();
-	void printChannelUtilization();
+	void printChannelInfo();
+	void printCurrentChannel();
 	void printSolution();
 };
 
 int main(){
+	cout << "Entering main()\n";
 	ChannelSwitching cs;
-	cs.printChannelUtilization();
+
+	cs.printCurrentChannel();
+	cs.printChannelInfo();
+
+	if (cs.fc4()){
+		cs.printSolution();
+	}else{
+		cout << "There is no solution\n";
+	}
 	return 0;
 }
 
@@ -85,71 +97,108 @@ ChannelSwitching::~ChannelSwitching(){
 	delete[] adjacent;
 }
 
+/** 
+ * @varIndex: variable index
+ * Return: true if the domain has been wiped out
+ */
 bool ChannelSwitching::dwo(int varIndex){
+	cout << "Entering dwo(" << varIndex << ")\n";
 	Domain *d = &domain[varIndex];
 	for (auto it = d->begin(); it != d->end(); ++it){
 		ChannelInfo *chanInfo = &(it->second);
-		if (chanInfo->mark == NOMARK)
-			return true;
+		if (chanInfo->mark == NOMARK){
+			cout << "Leaving dwo(" << varIndex << "): false" << "\n";
+			return false;
+		}
 	}
-	return false;
+	cout << "Leaving dwo(" << varIndex << "): true" << "\n";
+	return true;
 }
 
 void ChannelSwitching::restore(list<int> vars, int level){
+	cout << "Entering restore() at level " << level << '\n';
 	/* restore domain to previous state */
 	for (auto it = vars.begin(); it != vars.end(); ++it){
 		Domain *d = &(domain[*it]);
 		for (auto it2 = d->begin(); it2 != d->end(); ++it2){
-			if (it2->second.mark == level)
+			if (it2->second.mark == level){
+				cout << "restore domain of var " << *it << '\n';
 				it2->second.mark = NOMARK;
+			}
 		}
 	}
+	cout << "Leaving restore() at level " << level << '\n';
 }
 
 bool ChannelSwitching::searchFC4(list<int> vars, int level){
+	cout << "Entering searchFC4() at level " << level << '\n';
 	unsigned int Vi = vars.back();
+	cout << "selected var " << Vi << '\n';
 	vars.pop_back();
 	Domain *di = &domain[Vi];
 	for (auto it = di->begin(); it != di->end(); ++it){
 		ChannelInfo *chanInfo = &(it->second);
 		int chanNo = it->first;
 		if (chanInfo->mark == NOMARK){
+			cout << "add (Vi,vi)=(" << Vi << ',' << chanNo << ") to the solution\n";
 			solution.insert(pair<int,int>(Vi, chanNo));
-			if (Vi == nAP - 1)
+			if (Vi == 0){
 				/* found a solution */
+				cout << "Leaving searchFC4() at level " << level << ": last var\n";
 				return true;
+			}
 			else{
 				/* try to achieve partial arc-consistency */
 				if (checkForward4(vars, level, Vi, chanNo) &&
-					searchFC4(vars, level + 1))
+					searchFC4(vars, level + 1)){
+					cout << "Leaving searchFC4() at level " << level << ": true\n";
 					return true;
+				}
 				else{
+					cout << "remove var " << Vi << " from the solution\n";
 					solution.erase(Vi);
 					restore(vars, level);
 				}
 			}
 		}
 	}
+	cout << "Leaving searchFC4() at level " << level << ": false\n";
 	return false;
 }
 
+/**
+ * @varIndex: variable instantiated
+ * @value:	value assigned to the var at varIndex
+ * Return:	true all domains have not been wiped out
+ */
 bool ChannelSwitching::checkForward4(list<int> vars, int level, int varIndex, int value){
+	cout << "Entering checkForward4() at level " << level 
+		 << " with (" << varIndex << ',' << value << ")\n";
 	list<VarValuePair> *usp = &domain[varIndex][value].unsupportedSet;
+	cout << "unsupportedSet: \n";
 	for (auto it = usp->begin(); it != usp->end(); ++it){
 		int Vj = it->varIndex;
 		int vj = it->value;
+		cout << "(" << Vj << ',' << vj << "): ";
 		ChannelInfo *chanInfo = &domain[Vj][vj];
 		bool varsHasVj = find(vars.begin(), vars.end(), Vj) != vars.end();
 		if (varsHasVj && chanInfo->mark == NOMARK){
+			cout << "marked\n";
 			chanInfo->mark = level;
-			if (dwo(Vj))
+			if (dwo(Vj)){
+				cout << "Leaving checkForward4(): false\n";
 				return false;
+			}
 		}
+		cout << '\n';
 	}
+	cout << "Leaving checkForward4(): true\n";
 	return true;
 }
 
 bool ChannelSwitching::fc4(){
+	cout << "Entering fc4()\n";
+	cout << "unmask and clear unsupportedSet for all channel\n";
 	for (unsigned int i = 0; i < domain.size(); ++i){
 		Domain *d = &domain[i];
 		for (auto it = d->begin(); it != d->end(); ++it){
@@ -159,6 +208,7 @@ bool ChannelSwitching::fc4(){
 		}
 	}
 	solution.clear();
+	cout << "initialize unsupportedSet of all channels\n";
 	for (unsigned int i = 0; i < nAP; i++){
 		for (unsigned int j = i + 1; j < nAP; j++){
 			if (adjacent[i][j]){
@@ -171,12 +221,16 @@ bool ChannelSwitching::fc4(){
 							ChannelInfo *info2 = &it2->second;
 
 							VarValuePair vvp;
-							vvp.varIndex = i;
-							vvp.value = it->first;
-							info1->unsupportedSet.push_back(vvp);
-
 							vvp.varIndex = j;
 							vvp.value = it2->first;
+							//cout << "(" << i << ',' << it->first << ") usp + "
+							//	 << "(" << j << ',' << it2->first << ")\n";
+							info1->unsupportedSet.push_back(vvp);
+
+							vvp.varIndex = i;
+							vvp.value = it->first;
+							//cout << "(" << j << ',' << it2->first << ") usp + "
+							//	 << "(" << i << ',' << it->first << ")\n";
 							info2->unsupportedSet.push_back(vvp);
 						}
 					}
@@ -189,6 +243,7 @@ bool ChannelSwitching::fc4(){
 	for (unsigned int i = 0; i < domain.size(); ++i){
 		vars.push_back(i);
 	}
+	nodeConsistency();
 	return searchFC4(vars, 1);
 }
 
@@ -286,21 +341,52 @@ void ChannelSwitching::printTopo(){
 	cout << "Leaving ChannelSwitching::printTopo\n";
 }
 
-void ChannelSwitching::printChannelUtilization(){
-	cout << "Entering ChannelSwitching::printChannelUtilization()" << '\n';
+void ChannelSwitching::printChannelInfo(){
+	cout << "Entering ChannelSwitching::printChannelInfo()" << '\n';
 	for (unsigned int i = 0; i < nAP; i ++){
 		cout << "AP " << i << ":\t";
 		Domain *d = &domain[i];
 		for (auto it = d->begin(); it != d->end(); ++it){
 			ChannelUtilization *c = &(it->second.util);
-			cout << "(" << it->first << ", "
+			/* pattern: (chanNo, mark, envUtil, totalUtil) */
+			cout << "(" << it->first << ", " << it->second.mark << ", "
 				 << c->envUtil << ", " << c->totalUtil << ")\t";
 		}
 		cout << '\n';
 	}
-	cout << "Leaving ChannelSwitching::printChannelUtilization()\n";
+	cout << "Leaving ChannelSwitching::printChannelInfo()\n";
 }
 
 void ChannelSwitching::printSolution(){
-	
+	cout << "Entering printSolution()\n";
+	for (unsigned int i = 0; i < nAP; i++){
+		cout << "AP " << i << ": " << solution[i] << '\n';
+	}
+	cout << "Leaving printSolution()\n";
+}
+
+void ChannelSwitching::printCurrentChannel(){
+	cout << "Entering printCurrentChannel()\n";
+	for (unsigned int i = 0; i < nAP; i++){
+		cout << "AP " << i << ": " << currentChannel[i] << '\n';
+	}
+	cout << "Leaving printCurrentChannel()\n";
+}
+
+void ChannelSwitching::nodeConsistency(){
+	cout << "Entering nodeConsistency()\n";
+	for (unsigned int i = 0; i < nAP; ++i){
+		ChannelUtilization *currentChanUtil = &(domain[i][currentChannel[i]].util);
+		float currentBssUtil = currentChanUtil->totalUtil - currentChanUtil->envUtil;
+		Domain *d = &(domain[i]);
+		for (auto it = d->begin(); it != d->end(); ++it){
+			ChannelInfo *info = &(it->second);
+			if (currentBssUtil + info->util.envUtil >= LIMIT_UTIL){
+				/* mark removed */
+				cout << "remove value " << it->first << " from var " << i << '\n';
+				info->mark = REMOVED;
+			}
+		}
+	}
+	cout << "Leaving nodeConsistency()\n";
 }
